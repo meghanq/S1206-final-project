1a6e2c5d65409d9a18a39a9609e92a16a14dc19c
Lauren Belous <laurbel@umich.edu>
2022-04-07 11:20:13 -0400

    updated code

--- lauren_latlongBS.py
+++ lauren_latlongBS.py
@@ -21,18 +21,19 @@ def find_lat_long(city):
             print((float(lat),float(long)))
 find_lat_long('Herrin')
 
-def database_creation(db_filename):
+def database_creation(db_filename, website_url):
     full_path = os.path.join(os.path.dirname(__file__), db_filename)
     conn = sqlite3.connect(full_path)
     cur = conn.cursor()
 
-    cur.execute("DROP TABLE IF EXISTS cityInfo")
+    #cur.execute("DROP TABLE IF EXISTS cityInformation")
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS cityInfo (city TEXT, latitude FLOAT, longitude FLOAT)
+    CREATE TABLE IF NOT EXISTS cityInformation (city TEXT, latitude FLOAT, longitude FLOAT)
     """)
 
-    resp = requests.get('https://www.latlong.net/category/cities-236-15.html')
+    resp = requests.get(website_url)
+
     soup = BeautifulSoup(resp.text, 'html.parser')
     tr_tags = soup.find_all('tr')
     #print(tr_tags)
@@ -45,10 +46,20 @@ def database_creation(db_filename):
         print((float(lat),float(long)))
 
         cur.execute("""
-        INSERT INTO cityInfo (city, latitude, longitude)
+        INSERT INTO cityInformation (city, latitude, longitude)
         VALUES (?,?,?)
         """, (city_name[:-9], lat, long)) 
         conn.commit()
     
-database_creation('database.db')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-2.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-3.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-3.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-4.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-5.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-6.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-7.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-9.html')
+database_creation('database.db', 'https://www.latlong.net/category/cities-236-15-10.html')
+
 
