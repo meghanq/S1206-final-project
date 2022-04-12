7431fbb79d95467134bb2b676e19c9f069ce9f71
nclaer <nclaer@umich.edu>
2022-04-12 17:33:10 -0400

    Merge branch 'main' of https://github.com/meghanq/S1206-final-project

--- rec-areas.py
+++ rec-areas.py
@@ -120,19 +120,6 @@ def create_count_table(cur,conn,cities):
                 writer.writerow(data)
             except:
                 pass
-    #cur.execute('CREATE TABLE IF NOT EXISTS countNearCity (city_id INT, number INT)')
-    #for city in cities:
-        #longitude = get_long(cur,conn,city)
-        #latitude = get_lat(cur,conn,city)
-        #names = get_rec_data(longitude, latitude)
-        #try:
-            #count = len(names)
-            #cur.execute('SELECT city_id FROM CityQol WHERE name = ?', (city,))
-            #city_id = cur.fetchall()[0][0]
-            #cur.execute('INSERT OR IGNORE INTO countNearCity (city_id,number) VALUES(?,?)', (city_id,count))
-            #conn.commit()
-        #except:
-            #continue
     return None
 
 
