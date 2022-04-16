01486fd5628dd5439250f5e294495e22880fd341
nclaer <nclaer@umich.edu>
2022-04-16 19:27:31 -0400

    blocked the pie chart values

--- plot.py
+++ plot.py
@@ -78,14 +78,28 @@ def createPieChart1():
     full_path = os.path.join(base_path, "rec_count.csv")
     with open(full_path) as fileref:
         lines = fileref.readlines()
-        rec_count_lst = []
+        rec_count_lst = {}
+        rec_count_lst["0-4"] = 0
+        rec_count_lst["5-9"] = 0
+        rec_count_lst["10-14"] = 0
+        rec_count_lst["15-19"] = 0
+        rec_count_lst["20+"] = 0
         for line in lines[1:]:
             row = line.split(',')
             rec_count = int(row[1])
             qol_avg = float(row[2])
             if qol_avg > 6:
-                rec_count_lst.append(rec_count)
-    plt.pie(rec_count_lst, labels = rec_count_lst)
+                if rec_count <= 4 and rec_count >= 0:
+                    rec_count_lst["0-4"] += 1
+                elif rec_count <= 9 and rec_count >= 4:
+                    rec_count_lst["5-9"] += 1
+                elif rec_count <= 14 and rec_count >= 9:
+                    rec_count_lst["10-14"] += 1
+                elif rec_count <= 19 and rec_count >= 15:
+                    rec_count_lst["15-19"] += 1
+                else:
+                    rec_count_lst["20+"] += 1
+    plt.pie(list(rec_count_lst.values()), labels = list(rec_count_lst.keys()))
     plt.axis('equal')
     plt.title('Number of Rec Areas for QoL Ratings above 6')
     plt.show()
@@ -96,14 +110,29 @@ def createPieChart2():
     full_path = os.path.join(base_path, "rec_count.csv")
     with open(full_path) as fileref:
         lines = fileref.readlines()
-        rec_count_lst = []
+        rec_count_lst = {}
+        rec_count_lst = {}
+        rec_count_lst["0-2"] = 0
+        rec_count_lst["3-5"] = 0
+        rec_count_lst["6-8"] = 0
+        rec_count_lst["9-11"] = 0
+        rec_count_lst["12+"] = 0
         for line in lines[1:]:
             row = line.split(',')
             rec_count = int(row[1])
             qol_avg = float(row[2])
             if qol_avg < 5:
-                rec_count_lst.append(rec_count)
-    plt.pie(rec_count_lst, labels = rec_count_lst)
+                if rec_count <= 2 and rec_count >= 0:
+                    rec_count_lst["0-2"] += 1
+                elif rec_count <= 5 and rec_count >= 3:
+                    rec_count_lst["3-5"] += 1
+                elif rec_count <= 8 and rec_count >= 6:
+                    rec_count_lst["6-8"] += 1
+                elif rec_count <= 11 and rec_count >= 9:
+                    rec_count_lst["9-11"] += 1
+                else:
+                    rec_count_lst["12+"] += 1
+    plt.pie(list(rec_count_lst.values()), labels = list(rec_count_lst.keys()))
     plt.axis('equal')
     plt.title('Number of Rec Areas for QoL Ratings below 5')
     plt.show()
@@ -114,9 +143,9 @@ def createPieChart2():
 def main():
     create_scatter_plot()
     create_bar_chart()
-    # correlationCalc()
-    # createPieChart()
-    # createPieChart2()
+    correlationCalc()
+    createPieChart1()
+    createPieChart2()
 
 if __name__ == "__main__":
     main()
