039a96b1fdd2460290bb51be3e596165f7e88f75
Meghan Quigley <meghanq@umich.edu>
2022-04-19 16:34:18 -0400

    fixed average qol and took commas out of city names

--- QOL_calculations.py
+++ QOL_calculations.py
@@ -23,22 +23,15 @@ def get_city_avg(db_filename, city):
     conn = sqlite3.connect(path+'/'+db_filename)
     cur = conn.cursor()
 
-    cur.execute('''SELECT Housing_score, Living_Cost_score, Startup_score, Venture_Capital_score, Travel_score, Commute_score, Business_Freedom_score, 
-                    Safety_score, Healthcare_score, Education_score, Environmental_Quality_score, Economy_score, Taxation_score, Internet_Access_score, 
-                    Leisure_Culture_score, Tolerance_score, Outdoors_score FROM CityQoL WHERE name = ?''', (city,))
-    scores = cur.fetchall()
+    cur.execute('''SELECT (Housing_score + Living_Cost_score + Startup_score + Venture_Capital_score + Travel_score + Commute_score + Business_Freedom_score + 
+                    Safety_score + Healthcare_score + Education_score + Environmental_Quality_score + Economy_score + Taxation_score + Internet_Access_score + 
+                    Leisure_Culture_score + Tolerance_score + Outdoors_score) / 17 FROM CityQoL WHERE name = ?''', (city,))
+    avg = cur.fetchone()
+    print(city, avg)
 
-    total = 0
-    count = 0
-    for score in scores:
-        for num in score: 
-            total += float(num)
-            count += 1
+    return avg[0]
 
-    avg = total / count
-
-    return avg
-
+get_city_avg('database.db', 'Anchorage')
 
 
 def addAvgQoL(db_filename, city_dict):
@@ -60,11 +53,12 @@ def main():
     'Charlotte':'charlotte', 'Chicago':'chicago', 'Cincinnati':'cincinnati', 'Cleveland':'cleveland', 'Colorado Springs': 'colorado-springs', 'Columbus': 'columbus', 'Dallas': 'dallas', 'Denver': 'denver', 'Des Moines': 'des-moines', 'Detroit':'detroit', 'Honolulu': 'honolulu', 'Houston': 'houston', 
     'Indianapolis': 'indianapolis', 'Jacksonville': 'jacksonville', 'Kansas City': 'kansas-city', 'Knoxville':'knoxville', 'Las Vegas': 'las-vegas', 'Los Angeles': 'los-angeles', 'Louisville':'louisville', 'Madison':'madison', 'Memphis':'memphis', 'Miami':'miami', 'Milwaukee':'milwaukee', 
     'Minneapolis Saint-Paul': 'minneapolis-saint-paul', 'Nashville': 'nashville', 'New Orleans': 'new-orleans', 'New York':'new-york', 'Oklahoma City':'oklahoma-city', 'Omaha':'omaha', 'Orlando':'orlando', 'Palo Alto': 'palo-alto', 'Philadelphia':'philadelphia', 'Phoenix':'phoenix', 
-    'Pittsburgh':'pittsburgh', 'Portland, ME': 'portland-me', 'Portland, OR': 'portland-or', 'Providence':'providence', 'Raleigh':'raleigh', 'Richmond':'richmond', 'Rochester': 'rochester', 'Salt Lake City': 'salt-lake-city', 'San Antonio':'san-antonio', 'San Diego':'san-diego', 
+    'Pittsburgh':'pittsburgh', 'Portland ME': 'portland-me', 'Portland OR': 'portland-or', 'Providence':'providence', 'Raleigh':'raleigh', 'Richmond':'richmond', 'Rochester': 'rochester', 'Salt Lake City': 'salt-lake-city', 'San Antonio':'san-antonio', 'San Diego':'san-diego', 
     'San Francisco':'san-francisco-bay-area', 'Seattle': 'seattle', 'St. Louis': 'st-louis', 'Tampa Bay Area':'tampa-bay-area', 'Washington D.C.': 'washington-dc'}
 
-    createCityIdTable(city_name_dict, 'database.db')
+    # createCityIdTable(city_name_dict, 'database.db')
     addAvgQoL('database.db', city_name_dict)
+
 
 
 main()
