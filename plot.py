0ea6d988ae3b9beabc2297618793aff340f56c32
nclaer <nclaer@umich.edu>
2022-04-19 17:32:52 -0400

    removed sorted functions

--- plot.py
+++ plot.py
@@ -20,10 +20,8 @@ def create_scatter_plot():
                 avg_lst.append(rounded)
             else:
                 continue
-        sorted_rec_count = sorted(rec_count_lst)
-        sorted_avg = sorted(avg_lst)
 
-        plt.scatter(sorted_rec_count, sorted_avg)
+        plt.scatter(rec_count_lst, avg_lst)
         plt.xlabel('Count of Recreation Areas')
         plt.ylabel('Average Quality of Life Rating (Out of 10)')
         plt.title('Average Quality of Life vs Count of Recreation Areas')
@@ -46,9 +44,7 @@ def create_bar_chart():
                 avg_lst.append(rounded)
             else:
                 continue
-        sorted_rec_count = sorted(rec_count_lst)
-        sorted_avg = sorted(avg_lst)
-    plt.bar(sorted_rec_count, sorted_avg, color = 'g')
+    plt.bar(rec_count_lst, avg_lst, color = 'g')
     plt.xlabel('Count of Recreation Areas')
     plt.ylabel('Average Quality of Life Ratings (Out Of 10)')
     plt.title('Average Quality of Life Ratings vs Recreation Areas')
